fe0f7195c6aebb88e1d4bd2f85b5919e1a7b92af
maffei2443 <leoitu22hotmail.com@gmail.com>
2018-06-23 08:15:03 -0300

    exemplos collections

---
+++ examples/8_common_collections/string.rs
@@ -0,0 +1,9 @@
+// strings: coleção de bytes
+
+fn main() {
+	let st:&str = "string literal";
+	let St: String = String::from("conteudo dentro de um tipo String");
+	'x' == "x";
+	println!("{}", st);
+	println!("{}", St);
+}
